c3ba2a35795bea8174919baa24e1347bf25804da
Indra Rudianto <indrarudianto.official@gmail.com>
2021-08-10 14:51:53 +0700

    Remove config path checking

--- querybulletin/settings.py
+++ querybulletin/settings.py
@@ -1,5 +1,4 @@
 import os
-from .exceptions import ImproperlyConfigured
 
 USER_HOME = os.path.expanduser('~')
 BULLETIN_DIR = os.path.join(USER_HOME, '.bulletin')
@@ -8,10 +7,6 @@ if not os.path.isdir(CONFIG_DIR):
     os.makedirs(CONFIG_DIR)
 
 CONFIG_PATH = os.path.join(CONFIG_DIR, 'config.json')
-if not os.path.isfile(CONFIG_PATH):
-    raise ImproperlyConfigured(
-        'querybulletin config.json file is not found. Make sure you place it '
-        'in the ~/.bulletin/querybulletin/ directory.')
 
 DEFAULT_COLUMNS = [
     'eventid',
