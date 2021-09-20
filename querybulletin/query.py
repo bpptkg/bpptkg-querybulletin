4ca188b3736c2cb20b0125bf952f51600c272c1a
Indra Rudianto <indrarudianto.official@gmail.com>
2021-09-20 11:20:13 +0700

    Update function docstring

--- querybulletin/query.py
+++ querybulletin/query.py
@@ -52,6 +52,30 @@ def get_bulletin_modified_by_range(engine, table, starttime, endtime,
     """
     Get bulletin (only modified events) by particular time range and eventtype.
     If eventtype is None, query all events.
+
+    :param engine: SQLAlchemy engine.
+
+    :type engine: :class:`sqlalchemy.engine.base.Engine`
+
+    :param table: SQLAlchemy model or table.
+
+    :param starttime: Start time of the query.
+
+    :type starttime: datetime.datetime
+
+    :param endtime: End time of the query.
+
+    :type endtime: datetime.datetime
+
+    :param eventtype: Event type, e.g. VTA, VTB, MP. If you want to query
+    multiple eventtypes, pass a list or tuple. If eventtype is None, query all
+    events (excluding None).
+
+    :type eventtype: str, list, tuple, or None
+
+    :return: List of dictionary of the events.
+
+    :rtype: list
     """
     with session_scope(engine) as session:
         queryset = session.query(table).filter(
